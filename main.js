d44a02a1180fd53d797d6e527754c0c7be6d9460
mrkamin <108926875+mrkamin@users.noreply.github.com>
2023-07-20 22:48:02 +0500

    First concept Variables let and const

---
+++ main.js
@@ -0,0 +1,15 @@
+console.log('hellowrod form main.js')
+
+
+// with let onece initalized it can be reassigned
+let age = 32
+console.log(age)
+// const onec initalized can't reassigned
+const salary = 8000
+console.log(salary)
+
+// let sum  
+
+let sum = 0 
+sum = 8000
+console.log(sum)
